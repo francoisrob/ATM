7eac537bcfe8c6667a5fccd453a2a2271f9fddc1
Francois Robbertze <francoisdprob@gmail.com>
2022-06-20 01:30:17 +0200

    Initial

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
-#############################
-#                           #
-#   PYTHON ATM APPLICATION  #
-#                           #
-#############################
+"""
+                                        ============================
+                                        |  Python ATM Application  |
+                                        ============================
 
+
+Objectives:
+    * This program should have a gui with different frames/pages to access different parts of the program.
+    * There must be a login page for the user to log in with a Username and password.
+    * There must be a withdraw option.
+    * User should be able to view their funds.
+    * Should be secure, the program should take extra measures in encryption preventing any
+    unauthorized manipulation of data.
+
+Ideas:
+    * Fancy gui with animations, backgrounds, custom buttons etc.
+    * Widgets to resize to window dimensions.
+    *
+
+
+Glossary:
+
+*args allow us to pass a variable number of non-keyword arguments to a function, as a tuple
+**kwargs allow us to pass variable number of keyword arguments to a function, type= Dictionary
+source: https://tinyurl.com/yeyvstc3
+
+
+"""
 import tkinter as tk
 
-class MainWindow:
+class MainWindow(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        pass
+
+class LoginWindow:
     def __init__(self, master):
-        self.root = master
-        root.title('ATM')
-        root.geometry('500x500')
-        # self.hover = HoverInfo(self, 'Hello', self)
-
-        print('Hello world')
-
-    def ChangeColor(self):
-        print('hello dude')
+        pass
 
 
-root = tk.Tk()
-MainWindow(root)
-root.mainloop()
+if __name__ == "__main__":
+    root = MainWindow()
+    root.mainloop()
