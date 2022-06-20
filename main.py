aaa5374a043047a9c3d82de8a62dec8dcc4ab3d2
Francois Robbertze <francoisdprob@gmail.com>
2022-06-20 17:50:47 +0200

    Adding Frames

--- main.py
+++ main.py
@@ -24,19 +24,42 @@ Glossary:
 **kwargs allow us to pass variable number of keyword arguments to a function, type= Dictionary
 source: https://tinyurl.com/yeyvstc3
 
+The code to switch between different frames,
+source: https://tinyurl.com/bdedjxcy
 
 """
 import tkinter as tk
 
-class MainWindow(tk.Tk):
+class ATM_Application(tk.Tk):
     def __init__(self, *args, **kwargs):
-        pass
+        tk.Tk.__init__(self, *args, **kwargs)
 
-class LoginWindow:
-    def __init__(self, master):
-        pass
+        # The container will be where we stack the frames on top of
+        # The frames will be raised when called
+        container = tk.Frame(self)
+        container.pack(side='top', fill='both', expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_rowconfigure(0, weight=1)
+
+class MainMenu(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        Header_label = tk.Label(self, text='Hello')
 
 
 if __name__ == "__main__":
-    root = MainWindow()
+    root = ATM_Application()
     root.mainloop()
+
+
+"""
+==Most basic code to create a new frame
+
+class MainMenu(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        
+"""
