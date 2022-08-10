a33bec7a459bb0dee265fdf2e7a40d8d3841b1fa
Francois Robbertze <francoisdprob@gmail.com>
2022-08-10 15:30:36 +0200

    new

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ source: https://tinyurl.com/bdedjxcy
 import tkinter as tk
 import tkinter.font as tkfont
 import tkinter.ttk as ttk
-
+from PIL import Image, ImageTk
 
 class ATM_Application(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -41,13 +41,21 @@ class ATM_Application(tk.Tk):
 
         # The container will be where we stack the frames on top of
         # The frames will be raised when called
-        container = tk.Frame(self)
+        container = ttk.Frame(self)
         container.pack(side='top', fill='both', expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_rowconfigure(0, weight=1)
-
-
+        self.minsize(width=400, height=350)
+        self.geometry('500x500')
         self.frames = {}
+
+        # background image
+        width, height = self.winfo_screenwidth(), self.winfo_screenheight()
+        self.image = Image.open('background_image01.png')
+        self.image = self.image.resize((width, height))
+        self.bg_image = ImageTk.PhotoImage(self.image)
+        ttk.Label(self, image=self.bg_image).place(relx=.5, rely=.5, anchor='center')
+
         for F in (MainMenu, LoginPage):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
@@ -71,23 +79,55 @@ class LoginPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+        style = ttk.Style()
 
-        # GUI creation old tk :/
-        login_frame = tk.Frame(controller, bg='white')
-        login_frame.pack(expand=False, fill='y', side='top')
-        header_label = tk.Label(login_frame, text='Login to Your Account', font='Ubuntu 24', bg='white')
-        header_label.grid(row=0, column=0, padx=20, pady=(20, 40))
-        entry_username = tk.Entry(login_frame, relief='solid', font=controller.title_font)
+        # Variables
+        username = tk.StringVar()
+        password = tk.StringVar()
+
+        # GUI creation
+        style.configure('Custom.TFrame', background='white')
+
+        # Center Widget
+        self.login_frame = ttk.Frame(self.controller,
+                                     style='Custom.TFrame')
+        self.login_frame.place(relx=.5,
+                               rely=.5,
+                               anchor='center')
+
+        # Header Text
+        style.configure('Header.TLabel',
+                        background='white',
+                        font='Ubuntu 24 bold')
+        self.header_label = ttk.Label(self.login_frame,
+                                      text='Login to Your Account',
+                                      style='Header.TLabel')
+        self.header_label.grid(row=0, column=0, padx=20, pady=(20, 40))
+
+        # Input
+        style.configure('Subheader.TLabel',
+                        background='white',
+                        font='Ubuntu 12')
+        entry_username = tk.Entry(self.login_frame,
+                                  relief='solid',
+                                  font=controller.title_font,
+                                  textvariable=username)
         entry_username.grid(row=1, column=0, pady=10)
-        entry_password = tk.Entry(login_frame, relief='solid', show="*", font=controller.title_font)
+
+        entry_password = tk.Entry(self.login_frame,
+                                  relief='solid',
+                                  show="*",
+                                  font=controller.title_font,
+                                  textvariable=password)
         entry_password.grid(row=2, column=0, pady=10)
-        button_login = tk.Button(login_frame,
+
+        button_login = tk.Button(self.login_frame,
                                  text='SIGN IN',
                                  font=controller.title_font,
                                  relief='groove',
                                  bg='white',
                                  command=controller.show_frame)
-        button_login.grid(row=3, column=0, pady=(20,50))
+        button_login.grid(row=3, column=0, pady=(20, 50))
 
 
 if __name__ == "__main__":
