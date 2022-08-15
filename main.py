7f11040b7c44d547dc3aa9055fc5c40f4cf99d25
Corne <cornecalitz1@gmail.com>
2022-08-16 00:57:35 +0200

    Created login verification code: TROUBLE WITH SWITCHING FRAMES

--- main.py
+++ main.py
@@ -7,7 +7,10 @@
 Objectives:
     * This program should have a gui with different frames/pages to access different parts of the program.
     * There must be a login page for the user to log in with a Username and password.
-    * There must be a with-draw option.
+    * There must be a withdraw option. (How does one go about withdrawing cash from an online bank?)
+    * There must be a transfer option (Funds from the same user's account to another)
+    * There must be a pay outstanding credit button
+    * There must be a pay beneficiaries buttons (One user's account to another user's account)
     * User should be able to view their funds.
     * Should be secure, the program should take extra measures in encryption preventing any
     unauthorized manipulation of data.
@@ -31,20 +34,25 @@ source: https://tinyurl.com/bdedjxcy
 
 import tkinter as tk
 import tkinter.ttk as ttk
+from tkinter import messagebox
+
 import pyglet
 from PIL import Image, ImageTk
+
 import mysql.connector
 from mysql.connector import errorcode
 
-Version = 'v0.813'
+import re
+
+Version = 'v2-0813'
 pyglet.font.add_file('OpenSans.ttf')
 
 
-class Application(tk.Tk):
+class ATM_Application(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         self._frame = None
-        self.geometry('1000x700')
+        self.geometry('1000x600')
         self.title('National Bank')
         self.iconbitmap('favicon.ico')
 
@@ -54,7 +62,7 @@ class Application(tk.Tk):
         self.tk.call("source", "azure.tcl")
         self.tk.call("set_theme", "light")
 
-        # background image
+        # background image (Do not delete)
         width, height = self.winfo_screenwidth(), self.winfo_screenheight()
         self.image = Image.open('background_image01.png')
         self.image = self.image.resize((width, height))
@@ -102,82 +110,103 @@ class LoginPage(ttk.Frame):
         # GUI creation
         # Center Widget
         login_frame = ttk.Frame(self)
-        login_frame.grid()
-
-        self.image = Image.open('bank_logo.png')
-        self.image = self.image.resize((100, 50))
-        self.bg_image = ImageTk.PhotoImage(self.image)
-        ttk.Label(login_frame, image=self.bg_image).grid(row=0,
-                                                         column=0,
-                                                         pady=(20, 10),
-                                                         padx=20,
-                                                         sticky='n')
+        login_frame.pack(fill='none', expand=True, anchor='center')
 
         # Header Text
         self.header_label = ttk.Label(login_frame,
-                                      text='Log In', font=('Open Sans', 24))
-        self.header_label.grid(row=1,
+                                      text='Login to Your Account', font=('Open Sans', 20))
+        self.header_label.grid(row=0,
                                column=0,
                                padx=20,
-                               pady=(0, 15),
-                               sticky='w')
+                               pady=20,
+                               columnspan=2)
 
         # Input
         username_label = ttk.Label(login_frame,
                                    text='Username')
-        username_label.grid(row=2,
+        username_label.grid(row=1,
                             column=0,
-                            sticky='w',
-                            padx=20)
-        entry_username = ttk.Entry(login_frame,
-                                   textvariable=username,
-                                   justify='center',
-                                   width=30,
-                                   style='Custom.TEntry')
-        entry_username.grid(row=3,
-                            column=0,
-                            padx=20,
-                            pady=(0, 10),
-                            sticky='w')
+                            sticky='e')
+        self.entry_username = ttk.Entry(login_frame,
+                                        textvariable=username,
+                                        justify='center',
+                                        width=20,
+                                        style='Custom.TEntry')
+        self.entry_username.grid(row=1,
+                                 column=1,
+                                 pady=10,
+                                 padx=10,
+                                 sticky='w')
 
         password_label = ttk.Label(login_frame,
                                    text='Password')
-        password_label.grid(row=4,
+        password_label.grid(row=2,
                             column=0,
-                            sticky='w',
-                            padx=20)
-        entry_password = ttk.Entry(login_frame,
-                                   textvariable=password,
-                                   justify='center',
-                                   width=30,
-                                   show='*')
-        entry_password.grid(row=5,
-                            column=0,
-                            padx=20,
-                            pady=(0, 20),
-                            sticky='w')
+                            sticky='e')
+        self.entry_password = ttk.Entry(login_frame,
+                                        textvariable=password,
+                                        justify='center',
+                                        width=20,
+                                        show='*')
+        self.entry_password.grid(row=2,
+                                 column=1,
+                                 pady=10,
+                                 padx=10,
+                                 sticky='w')
 
         # Login in button, currently only show the next frame, no verification function yet
         button_login = ttk.Button(login_frame,
                                   text='SIGN IN',
-                                  width=29,
+                                  width=35,
                                   style='Accent.TButton',
-                                  command=lambda: master.switch_frame(MainMenu))
-        button_login.grid(row=6,
+                                  command=lambda: self.check_login_details(self.entry_username.get(),
+                                                                           self.entry_password.get()))
+        button_login.grid(row=3,
                           column=0,
-                          padx=20,
-                          pady=10,
+                          pady=(50, 20),
                           columnspan=2)
 
-        button_register = ttk.Button(login_frame,
-                                     text='Register',
-                                     width=29,
-                                     style='Panel.TButton',
-                                     command=lambda: master.switch_frame(MainMenu))
-        button_register.grid(row=7,
-                             padx=20,
-                             pady=(0, 10),
-                             columnspan=2)
+        """DB check for username and password"""
+
+    @staticmethod
+    def check_login_details(username, password):
+
+        # No username and password entered
+        if not username:
+            messagebox.showerror("Invalid entry", "Username cannot be left blank.\nPlease enter a Username.")
+        elif not password:
+            messagebox.showerror("Invalid entry", "Password cannot be left blank.\nPlease enter a Password.")
+
+        # Username and password entered
+        else:
+            db = db_connect()
+            db_cursor = db.cursor()
+            db_cursor.execute("SELECT username, password FROM db_atm.tbl_users")
+            users = db_cursor.fetchall()
+            user_input = password
+            user_password = ""
+
+            for user in users:
+                if user[0] == username:
+                    user_input = password  # Fetches the typed password
+                    user_password = user[1]  # Fetches the correct password
+                    # Reason for this is because I do not want an error message running on each unmatched entry
+
+            if user_password == "":
+                # Warning message displayed for unmatched entry form db
+                messagebox.showerror("Invalid entry", "Username or password is incorrect")
+            elif user_input == user_password:
+                ATM_Application().switch_frame(MainMenu)
+                """<--------------- BIG ERROR HERE"""
+            else:
+                # Password is incorrect and does not match username
+                messagebox.showerror("Invalid entry", "Username or password is incorrect")
+
+            # You can be specific and tell the user what is wrong with their inputs.
+            # The reason I want to leave it ambiguous is, because anybody can guess a username and in the instance
+            # where they guess correctly they can attempt to crack the password.
+            # (Discuss which approach to follow)
+
 
 
 class MainMenu(ttk.Frame):
@@ -198,10 +227,11 @@ class MainMenu(ttk.Frame):
         self._panel = None
         self.show_panel(AccountsPanel)
 
-        self.header_panel = ttk.Panedwindow(self.left_panel, style='Card.TFrame')
-        self.header_panel.pack(side='top', fill='x')
-        self.header_label = ttk.Label(self.header_panel, text='Welcome', font=('Open Sans', 20))
+        self.header_label = ttk.Label(self.left_panel,
+                                      text='Welcome',
+                                      font=('Open Sans', 30))
         self.header_label.pack(side='top',
+                               padx=(20, 30),
                                pady=(10, 20))
         self.accounts_button = ttk.Button(self.left_panel,
                                           text='Accounts',
@@ -229,13 +259,11 @@ class MainMenu(ttk.Frame):
         self.logout_button.pack(side='bottom', fill='x')
 
         # theme button
-        theme_button = ttk.Checkbutton(self.left_panel,
-                                       command=master.set_theme,
-                                       text='Theme',
-                                       style="Switch.TCheckbutton")
-        theme_button.pack(side='bottom', fill='x', padx=60)
-        if self.tk.call("ttk::style", "theme", "use") == "azure-dark":
-            theme_button.state(['selected'])
+        theme_button = ttk.Button(self.left_panel,
+                                  command=master.set_theme,
+                                  text='Theme',
+                                  style="Panel.TButton")
+        theme_button.pack(side='bottom', fill='x')
 
     def show_panel(self, panel):
         # This function displays new panels on the right
@@ -243,14 +271,14 @@ class MainMenu(ttk.Frame):
         if self._panel is not None:
             self._panel.destroy()
         self._panel = new_panel
-        self._panel.pack()
+        self._panel.grid()
 
 
 class AccountsPanel(ttk.Frame):
     def __init__(self, master):
         ttk.Frame.__init__(self, master)
         # Gui Creation
-        self.accounts_panel = ttk.Frame(self, width=600, height=600, style="Card.TFrame")
+        self.accounts_panel = ttk.Frame(self, width=500, height=500, style="Card.TFrame")
         self.accounts_panel.grid()
         self.accounts_panel.grid_propagate(False)
 
@@ -274,37 +302,22 @@ class CardsPanel(ttk.Frame):
     def __init__(self, master):
         ttk.Frame.__init__(self, master)
         # Gui Creation
-        self.cards_panel = ttk.Frame(self, width=600, height=600, style="Card.TFrame")
+        self.cards_panel = ttk.Frame(self, width=500, height=500, style="Card.TFrame")
         self.cards_panel.grid()
         self.cards_panel.grid_propagate(False)
 
-        # ttk.Label(self.cards_panel, text="Cards").grid(column=0,
-        #                                                row=0,
-        #                                                padx=30,
-        #                                                pady=(30, 0),
-        #                                                sticky='w')
-        self.credit_panel = ttk.Frame(self.cards_panel,
-                                      width=600,
-                                      height=200,
-                                      style='Card.TFrame')
-        self.credit_panel.grid(row=0, column=0, padx=1)
-        self.debit_panel = ttk.Frame(self.cards_panel,
-                                     width=600,
-                                     height=200,
-                                     style='Card.TFrame')
-        self.debit_panel.grid(row=1, column=0, padx=1)
-        self.savings_panel = ttk.Frame(self.cards_panel,
-                                       width=600,
-                                       height=200,
-                                       style='Card.TFrame')
-        self.savings_panel.grid(row=2, column=0, padx=1)
+        ttk.Label(self.cards_panel, text="Cards").grid(column=0,
+                                                       row=0,
+                                                       padx=30,
+                                                       pady=(30, 0),
+                                                       sticky='w')
 
 
 class PaymentsPanel(ttk.Frame):
     def __init__(self, master):
         ttk.Frame.__init__(self, master)
         # Gui Creation
-        self.payments_panel = ttk.Frame(self, width=600, height=600, style="Card.TFrame")
+        self.payments_panel = ttk.Frame(self, width=500, height=500, style="Card.TFrame")
         self.payments_panel.grid()
         self.payments_panel.grid_propagate(False)
 
@@ -347,6 +360,26 @@ def db_connect():
     # End of borrowed code
 
 
+# This can be used later on
+"""Validation for username and password
+    def error_check_username(self, username):
+        # Checks if the received username matches the required conditions.
+
+        # Usernames can't be shorter than 1 character
+        if len(username) < 1:
+            messagebox.showerror("Invalid username", "Username must be longer than 1 character")
+            return False
+        # Usernames can only use letters, numbers, dots, underscores and hyphens
+        if not re.match('^[a-z0-9._-]*$', username):
+            messagebox.showerror("Invalid username", "u")
+            return False
+        # Usernames can't begin with a non-alphanumeric character
+        if not username[0].isalpha():
+            messagebox.showerror("Invalid username", "Username must begin with a letter")
+            return False
+        return True
+"""
+
 if __name__ == "__main__":
-    root = Application()
+    root = ATM_Application()
     root.mainloop()
